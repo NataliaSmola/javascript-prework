be26e8910213542f672c6c578f3042413d274b46
Natalia Smola <smolutka15@gmail.com>
2020-10-02 19:53:05 +0200

    Add if  and else if function in js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+//computerMove
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -16,6 +26,7 @@ else if (randomNumber == 3) {
 
 printMessage('Mój ruch to: ' + computerMove);
 
+// playerMove & prompt
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -34,15 +45,28 @@ else if (playerInput == '3') {
 
 printMessage('Twój ruch to: ' + playerMove);
 
-
-
-
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+// Game results
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+else if (computerMove == 'papier' && playerMove == 'kamień') {
+printMessage('Przegrałes')
+}
+else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+printMessage('Ty wygrywasz')
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+printMessage('Przegrałes')
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier') {
+printMessage('Przegrałes')
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce') {
+printMessage('Ty wygrywasz')
+}
+else if (computerMove == playerMove) {
+  printMessage('Remis!')
+}
+else {
+  printMessage('Spróbuj jeszcze raz!')
 }
